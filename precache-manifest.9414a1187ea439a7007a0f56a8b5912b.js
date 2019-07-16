237b35cd2cb014c37c97b473536e7e64156f232c
Marcin Olszowski <olszyna38@gmail.com>
2019-07-16 22:00:26 +0200

    Updates

---
+++ precache-manifest.9414a1187ea439a7007a0f56a8b5912b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f1b7ff904f4123b72bab842186590d6",
+    "url": "/ecommerce-app/index.html"
+  },
+  {
+    "revision": "1803155597a342390c08",
+    "url": "/ecommerce-app/static/css/main.8d66b01c.chunk.css"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/ecommerce-app/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "1803155597a342390c08",
+    "url": "/ecommerce-app/static/js/main.45262d4a.chunk.js"
+  },
+  {
+    "revision": "fe4bda9b75df12e07ad5",
+    "url": "/ecommerce-app/static/js/runtime~main.5b6ba8c6.js"
+  }
+]);
